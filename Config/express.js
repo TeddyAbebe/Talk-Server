e199e2ad4303567aaa8e1665956f4cb7ed5179e3
Tewodros Abebe <teddyab729@gmail.com>
2023-12-06 17:43:41 +0300

    Update

--- Config/express.js
+++ Config/express.js
@@ -1,6 +1,8 @@
 const cors = require("cors");
+const express = require("express");
 
 module.exports = function (app) {
   // Middlewares
   app.use(cors());
+  app.use(express.json());
 };
--- Routes/UserRoutes/index.js
+++ Routes/UserRoutes/index.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const router = express.Router();
+const { registerUser } = require("../../Controllers/UserControllers");
 
-// router.route("/").post(registerUser);
+router.route("/").post(registerUser);
 // router.route("/login", authUser)
 
 module.exports = router;
