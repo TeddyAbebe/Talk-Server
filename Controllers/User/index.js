34f43e2a89e81a3cfc7f725948fde521f50a34b9
Tewodros Abebe <teddyab729@gmail.com>
2024-01-17 18:07:53 +0300

    .

--- Controllers/User/index.js
+++ Controllers/User/index.js
@@ -64,7 +64,7 @@ const allUsers = asyncHandler(async (req, res) => {
     : {};
 
   const users = await User.find(keyword).find({ _id: { $ne: req.user._id } });
-  console.log(users);
   res.send(users);
 });
+
 module.exports = { registerUser, loginUser, allUsers };
